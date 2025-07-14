03cec1abda79cbe6da2532e150f6c0de93f65bc6
NewbDev-gif <rhobertcarwana@gmail.com>
2025-07-14 21:45:59 +0800

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_SqvFJ2qT_FnpAbZXBfnFcHXhmeFU3xV3g);
 
 export default async function handler(req, res) {
   const { name, email, message } = req.body;
